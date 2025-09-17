209148473fba4754ed3b03d60d479740cb18976a
Maurício Noal <noal.rodrigo10@gmail.com>
2025-09-16 21:34:48 -0300

    Gráfico_com_Python_e_Matplotlib.py

--- projeto.py
+++ projeto.py
@@ -1,11 +1,24 @@
-num = ("Viajem ao centro da terra","Júlio Verne","Ficção Cientifica","40 livros")
-print(num)
-num1 = ("O Hobbit","Jr Tolkien","Ficção Cientifica","30 livros")
-print(num1)
-num2 = ("Moby Dick","Frankenstein","Drácula")
-print(num2)
-num3 = ("Jane Eyre",)
-print(num3)
-all_books = num + num1 + num2 + num3
-print(all_books)
+livro = ("O Hobbit", "JR.Tolkien", "Ficção Cientifica", "30")
+livro2 = ("Frankenstein", "Mary Shelley", "Ficção Cientifica", "25")
+livro3 = ("Moby Dick", "Herman Melville", "Ficção Cientifica", "20")
+lista_mista = [livro , livro2 , livro3]
+print(lista_mista)
+novo_livro = ("Jane Eyre", "Charlotte Bronte", "Romance", "15")
+print(novo_livro)
+print(livro[0])
 
+import matplotlib.pyplot as plt
+
+meses = ['O Hobbit', 'Frankenstein', 'Moby Dick', 'Jane Eyre']
+
+vendas = [30, 25, 20, 15]
+
+plt.bar(meses, vendas, color='royalblue')
+
+plt.xlabel('Ficção Cientifica        x        Romance')
+
+plt.ylabel('Vendas (em unidades)')
+
+plt.title('Ficção Cientifica 75 x 15 Romance')
+
+plt.show()
